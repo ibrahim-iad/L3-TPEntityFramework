7dc6e037a6e66c73c1dba8ea3f761e803895a1b9
Ibrahim Mohamed <ibroman79@gmail.com>
2023-05-13 21:30:48 +0300

    Ajoutez des fichiers projet.

---
+++ TPEntityFramework/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace TPEntityFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSql1 = new System.Windows.Forms.Button();
+            this.btnSql2 = new System.Windows.Forms.Button();
+            this.btnSql3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSql4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSql5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSql8 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSql7 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnSql6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSql1
+            // 
+            this.btnSql1.Location = new System.Drawing.Point(13, 24);
+            this.btnSql1.Name = "btnSql1";
+            this.btnSql1.Size = new System.Drawing.Size(161, 47);
+            this.btnSql1.TabIndex = 0;
+            this.btnSql1.Text = "Requete SQL";
+            this.btnSql1.UseVisualStyleBackColor = true;
+            this.btnSql1.Click += new System.EventHandler(this.btnSql1_Click);
+            // 
+            // btnSql2
+            // 
+            this.btnSql2.Location = new System.Drawing.Point(523, 24);
+            this.btnSql2.Name = "btnSql2";
+            this.btnSql2.Size = new System.Drawing.Size(161, 47);
+            this.btnSql2.TabIndex = 1;
+            this.btnSql2.Text = "Requete Linq1";
+            this.btnSql2.UseVisualStyleBackColor = true;
+            this.btnSql2.Click += new System.EventHandler(this.btnSql2_Click);
+            // 
+            // btnSql3
+            // 
+            this.btnSql3.Location = new System.Drawing.Point(523, 89);
+            this.btnSql3.Name = "btnSql3";
+            this.btnSql3.Size = new System.Drawing.Size(161, 47);
+            this.btnSql3.TabIndex = 2;
+            this.btnSql3.Text = "Requete Linq2";
+            this.btnSql3.UseVisualStyleBackColor = true;
+            this.btnSql3.Click += new System.EventHandler(this.btnSql3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(691, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "tous les contacts";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(690, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "tous les contacts avec le nom du groupe";
+            // 
+            // btnSql4
+            // 
+            this.btnSql4.Location = new System.Drawing.Point(523, 154);
+            this.btnSql4.Name = "btnSql4";
+            this.btnSql4.Size = new System.Drawing.Size(161, 47);
+            this.btnSql4.TabIndex = 5;
+            this.btnSql4.Text = "Requete Linq3";
+            this.btnSql4.UseVisualStyleBackColor = true;
+            this.btnSql4.Click += new System.EventHandler(this.btnSql4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(691, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "les contacts du groupe Amis";
+            // 
+            // btnSql5
+            // 
+            this.btnSql5.Location = new System.Drawing.Point(523, 219);
+            this.btnSql5.Name = "btnSql5";
+            this.btnSql5.Size = new System.Drawing.Size(161, 47);
+            this.btnSql5.TabIndex = 7;
+            this.btnSql5.Text = "Requete Linq4";
+            this.btnSql5.UseVisualStyleBackColor = true;
+            this.btnSql5.Click += new System.EventHandler(this.btnSql5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(691, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "les contacts du groupe Amis et Travail";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(181, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "les contacts du groupe Amis et Travail";
+            // 
+            // btnSql8
+            // 
+            this.btnSql8.Location = new System.Drawing.Point(13, 219);
+            this.btnSql8.Name = "btnSql8";
+            this.btnSql8.Size = new System.Drawing.Size(161, 47);
+            this.btnSql8.TabIndex = 13;
+            this.btnSql8.Text = "Requete SQL4";
+            this.btnSql8.UseVisualStyleBackColor = true;
+            this.btnSql8.Click += new System.EventHandler(this.btnSql8_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(181, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(140, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "les contacts du groupe Amis";
+            // 
+            // btnSql7
+            // 
+            this.btnSql7.Location = new System.Drawing.Point(13, 154);
+            this.btnSql7.Name = "btnSql7";
+            this.btnSql7.Size = new System.Drawing.Size(161, 47);
+            this.btnSql7.TabIndex = 11;
+            this.btnSql7.Text = "Requete SQL3";
+            this.btnSql7.UseVisualStyleBackColor = true;
+            this.btnSql7.Click += new System.EventHandler(this.btnSql7_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(180, 89);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(199, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "tous les contacts avec le nom du groupe";
+            // 
+            // btnSql6
+            // 
+            this.btnSql6.Location = new System.Drawing.Point(13, 89);
+            this.btnSql6.Name = "btnSql6";
+            this.btnSql6.Size = new System.Drawing.Size(161, 47);
+            this.btnSql6.TabIndex = 9;
+            this.btnSql6.Text = "Requete SQL2";
+            this.btnSql6.UseVisualStyleBackColor = true;
+            this.btnSql6.Click += new System.EventHandler(this.btnSql6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 391);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSql8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSql7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnSql6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSql5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSql4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSql3);
+            this.Controls.Add(this.btnSql2);
+            this.Controls.Add(this.btnSql1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSql1;
+        private System.Windows.Forms.Button btnSql2;
+        private System.Windows.Forms.Button btnSql3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSql4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSql5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSql8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSql7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSql6;
+    }
+}
+
---
+++ TPEntityFramework/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPEntityFramework
+{
+    public partial class Form1 : Form
+    {
+        mescontactsEntities context;
+        public Form1()
+        {
+            InitializeComponent();
+            context = new mescontactsEntities();
+        }
+
+        private void btnSql1_Click(object sender, EventArgs e)
+        {
+            //mescontactsEntities context = new mescontactsEntities();
+            var contacts = context.contacts.SqlQuery("SELECT * FROM contacts");
+            foreach(contact c in contacts)
+            {
+                MessageBox.Show("Nom : " + c.nom);
+            }
+        }
+
+        private void btnSql2_Click(object sender, EventArgs e)
+        {
+            //mescontactsEntities context = new mescontactsEntities();
+            var contacts = context.contacts.ToList();
+            foreach (contact c in contacts)
+            {
+                MessageBox.Show("Nom: " + c.nom);
+            }
+        }
+
+        private void btnSql3_Click(object sender, EventArgs e)
+        {
+            //mescontactsEntities context = new mescontactsEntities();
+            var contacts = context.contacts.ToList();
+            foreach (contact c in contacts)
+            {
+                MessageBox.Show("Nom : " + c.nom + "\nGroupe : " + c.groupe.nom);
+            }
+        }
+
+        private void btnSql4_Click(object sender, EventArgs e)
+        {
+            //mescontactsEntities context = new mescontactsEntities();
+            var contacts = context.contacts.Where(c => c.groupe.nom.Equals("Amis"));
+            foreach (contact c in contacts)
+            {
+                MessageBox.Show("Nom : " + c.nom + "\nGroupe : " + c.groupe.nom);
+            }
+        }
+
+        private void btnSql5_Click(object sender, EventArgs e)
+        {
+            //mescontactsEntities context = new mescontactsEntities();
+            var mesgroupes = new List<string> { "Travail", "Amis" };
+            var contacts = context.contacts.Where(c => mesgroupes.Contains(c.groupe.nom)).ToList();
+            foreach (contact c in contacts)
+            {
+                MessageBox.Show("Nom : " + c.nom + "\nGroupe : " + c.groupe.nom);
+            }
+        }
+
+        private void btnSql6_Click(object sender, EventArgs e)
+        {
+            //mescontactsEntities context = new mescontactsEntities();
+            var contacts = context.contacts.SqlQuery("SELECT * FROM contacts JOIN groupes ON contacts.groupe_id=groupes.id");
+            foreach (contact c in contacts)
+            {
+                MessageBox.Show("Nom : " + c.nom + "\nGroupe : " + c.groupe.nom);
+            }
+        }
+
+        private void btnSql7_Click(object sender, EventArgs e)
+        {
+            //mescontactsEntities context = new mescontactsEntities();
+            var contacts = context.contacts.SqlQuery("SELECT * FROM contacts JOIN groupes ON contacts.groupe_id = groupes.id" +
+                " WHERE groupes.nom='Amis'");
+            foreach (contact c in contacts)
+            {
+                MessageBox.Show("Nom : " + c.nom + "\nGroupe : " + c.groupe.nom);
+            }
+        }
+
+        private void btnSql8_Click(object sender, EventArgs e)
+        {
+            //mescontactsEntities context = new mescontactsEntities();
+            var contacts = context.contacts.SqlQuery("SELECT * FROM contacts JOIN groupes ON contacts.groupe_id = groupes.id " +
+                "WHERE groupes.nom IN('Amis','Travail')");
+            foreach (contact c in contacts)
+            {
+                MessageBox.Show("Nom : " + c.nom + "\nGroupe : " + c.groupe.nom);
+            }
+        }
+    }
+}
